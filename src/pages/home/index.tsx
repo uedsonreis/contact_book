62969b124d8f9df567bcb4ec636c247c7756518f
Uedson Reis <uedsonreis@gmail.com>
2020-05-25 18:42:18 -0300

    First commit.

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component, ReactNode } from "react";
+import { Button, Text, View } from "react-native";
+
+export default class HomePage extends Component<any, any> {
+
+    render(): ReactNode {
+        return (
+            <View>
+            </View>
+        )
+    }
+
+}
