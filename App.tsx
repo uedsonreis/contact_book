d7399cfc46b0f992273c0e1633ce443ae151f5e0
Uedson Reis <uedsonreis@gmail.com>
2020-06-01 18:46:10 -0300

    Seventh day of class.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { persistor, store } from './src/redux/store'
 import Router from './src/router'
 
-export default function App() {
+export default function App(): ReactNode {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
